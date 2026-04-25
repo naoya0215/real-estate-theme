4264385b2ce0914ddebd3b680f3178128ee1ca35
naoya0215 <purgatory0215@icloud.com>
2026-04-25 22:25:25 +0900

    検索機能・問い合わせフォーム(文言修正)

--- page-contact.php
+++ page-contact.php
@@ -3,7 +3,7 @@
 <main class="contact">
     <div class="container">
         <h1>お問い合わせ</h1>
-        <p class="contact__lead">物件に関するご質問・内見のご予約など、お気軽にお問い合わせください。</p>
+        <p class="contact__lead">物件に関するご質問など、お気軽にお問い合わせください。</p>
 
         <div class="contact__form">
             <?php echo do_shortcode('[contact-form-7 id="1937be2" title="物件お問い合わせ"]'); ?>
